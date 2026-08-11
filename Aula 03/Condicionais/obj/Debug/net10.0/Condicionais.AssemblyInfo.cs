1197add2b6ba1475833145ccde0277c451a463d0
thevitordevyt-dotcom <thevitordevyt@gmail.com>
2026-08-11 10:30:08 -0300

    Aula 05: Exercicios

--- Aula 03/Condicionais/obj/Debug/net10.0/Condicionais.AssemblyInfo.cs
+++ Aula 03/Condicionais/obj/Debug/net10.0/Condicionais.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d52e40c1cec1b871e164e7cd421a995631a704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c5697e48cdb4b803af1cfde5fd7b5610de97ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula-04/04 - Polimosfismo/Polimorfismo/obj/Debug/net10.0/Polimorfismo.AssemblyInfo.cs
+++ Aula-04/04 - Polimosfismo/Polimorfismo/obj/Debug/net10.0/Polimorfismo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Polimorfismo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d52e40c1cec1b871e164e7cd421a995631a704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c5697e48cdb4b803af1cfde5fd7b5610de97ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Polimorfismo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Polimorfismo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
